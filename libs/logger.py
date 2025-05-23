d56174468a2cd106980479d64a8e4317aeb9d2dd
Edgardo Ortiz <edgardo.ortiz@alumnos.ucn.cl>
2025-05-22 20:54:36 -0400

    (docs): Se añadió la documentación markdown a ambos notebooks y se hicieron algunos arreglos menores en archivos secundarios.

--- libs/logger.py
+++ libs/logger.py
@@ -1,12 +1,12 @@
 #  Copyright (c) 2025.  Departamento de Ingenieria de Sistemas y Computacion
 import logging
-
+from typing import Union
 import coloredlogs
 from typeguard import typechecked
 
 
 @typechecked
-def configure_logging() -> None:
+def configure_logging(log_level: Union[int, str] = logging.INFO) -> None:
     log_level = logging.DEBUG
     log_format = \
         "%(asctime)s [%(levelname)8s] %(name)s:%(lineno)d (%(process)d/%(threadName)s)  - %(message)s"
